b14353f1c1d11ba056978426fb06b7faf7f25e60
caioc33 <163444609+caioc33@users.noreply.github.com>
2024-03-15 16:21:23 +0000

    Exercícios

--- exercicios/2-construindo-consultas/5-funcoes-agregadoras.sql
+++ exercicios/2-construindo-consultas/5-funcoes-agregadoras.sql
@@ -1,1 +1,12 @@
 -- Para a tabela "invoices", crie uma consulta que retorne o país e a soma das compras realizadas nele, bem como o valor da compra mínima e máxima, a quantidade de compras realizadas e o ticket médio de cada compra.
+SELECT
+BillingCountry as País,
+SUM(Total) as Soma_das_Compras,
+COUNT(Total) as Qnt_de_Compras_Realizadas,
+MIN(Total) as Menor_Compra,
+MAX(Total) as Maior_Valor,
+ROUND (AVG(Total),2) as Ticket_Médio
+FROM
+invoices
+WHERE
+BillingCountry = 'Brazil';
